d63d369592d5af7b8d2f59a722ddb447d7741205
Ahmet Yanık <ahmetyaniklar@gmail.com>
2023-09-19 16:10:34 +0300

    Update 2

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -31,16 +31,17 @@
                     <Icon name="ic:sharp-install-mobile" size="17"/>
                     App
                 </li>
-                <ul 
+                <li 
                     @mouseenter="isAccountMenu = true"
                     @mouseleave="isAccountMenu = false"
                     class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
                     :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]' : 'border border-[#FAFAFA]'"
                 >
-                <Icon name="ph:user-thin" size="17"/>
+                    <Icon name="ph:user-thin" size="17"/>
                     Account
-                <Icon name="mdi:chevron-down" size="15" class="ml-5"/>
-                <div 
+                    <Icon name="mdi:chevron-down" size="15" class="ml-5"/>
+
+                    <div 
                         id="AccountMenu" 
                         v-if="isAccountMenu"
                         class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
@@ -57,7 +58,7 @@
                             </div>
                         </div>
                         <div class="border-b"/>
-                        <ul class="bg-white">
+                        <ul class="bg-white ">
                             <li 
                                 @click="navigateTo('/orders')"
                                 class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
@@ -72,15 +73,107 @@
                                 Sign out
                             </li>
                         </ul>
+                        
                     </div>
-            </ul>
+                </li>
             </ul>
         </div>
+        <div 
+            id="MainHeader" 
+            class="flex items-center w-full bg-white"
+        >
+            <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
+                <NuxtLink to="/" class="min-w-[170px]">
+                    <img 
+                        width="170"
+                        src="/AliExpress-logo.png"
+                    >
+                </NuxtLink>
+
+                <div class="max-w-[700px] w-full md:block hidden">
+                    <div class="relative">
+                        <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+                            <input 
+                                class="
+                                    w-full
+                                    placeholder-gray-400
+                                    text-sm
+                                    pl-3
+                                    focus:outline-none
+                                "
+                                placeholder="kitchen accessories"
+                                type="text"
+                                v-model="searchItem"
+                            >
+                            <Icon 
+                                v-if="isSearching" 
+                                name="eos-icons:loading" 
+                                size="25" 
+                                class="mr-2"
+                            />
+                            <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]">
+                                <Icon name="ph:magnifying-glass" size="20" color="#ffffff"/>
+                            </button>
+                        </div>
+
+                        <div class="absolute bg-white max-w-[700px] h-auto w-full">
+                            <div 
+                                v-if="items && items.data" 
+                                v-for="item in items.data" 
+                                class="p-1"
+                            >
+                                <NuxtLink 
+                                    :to="`/item/${item.id}`" 
+                                    class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                                >
+                                    <div class="flex items-center">
+                                        <img class="rounded-md" width="40" :src="item.url">
+                                        <div class="truncate ml-2">{{ item.title }}</div>
+                                    </div>
+                                    <div class="truncate">${{ item.price / 100 }}</div>
+                                </NuxtLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <Loading v-if="userStore.isLoading" />
+
+    <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]" />
+    <slot />
+    <Footer v-if="!userStore.isLoading"/>
 
     </div>
 </template>
 
 <script setup>
-let isAccountMenu =ref(false)
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+
+
+let isAccountMenu = ref(false)
+let isCartHover = ref(false)
+let isSearching = ref(false)
+let searchItem = ref('')
+let items = ref(null)
+
+const searchByName = useDebounce(async () => {
+    isSearching.value = true
+    items.value = await useFetch(`/api/prisma/search-by-name/${searchItem.value}`)
+    isSearching.value = false
+}, 100)
+
+watch(() => searchItem.value, async () => {
+    if (!searchItem.value) { 
+        setTimeout(() => {
+            items.value = ''
+            isSearching.value = false
+            return
+        }, 500)
+    }
+    searchByName() 
+})
 </script>
 
